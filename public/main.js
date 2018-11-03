c75386aee35b3dd673c9f696879ba36ef1451434
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-02 23:07:57 -0500

    added first helper function

---
+++ public/main.js
@@ -0,0 +1,12 @@
+//====== Events
+
+// Run content when document is loaded
+
+var loaded = function() {
+  document.addEventListener('DOMContentLoaded', function() {
+  //Do whatever you want
+  console.log('Hello');
+  });
+}
+
+loaded();
